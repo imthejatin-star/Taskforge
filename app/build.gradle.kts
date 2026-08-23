6e1510a04b74380430071fb9b94a588d0dcbfeb4
imthejatin-star <imthejatin@gmail.com>
2026-08-23 17:05:09 +0530

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.taskforge.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.taskforge.app"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 2
+        versionName = "2.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+}
